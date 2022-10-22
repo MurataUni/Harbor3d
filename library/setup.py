ec93ef30ee79ad4dd392af9f6b6286e4d4cb75ac
Murata Uni <c.murata.uni@gmail.com>
2022-10-22 18:24:24 +0900

    ver up to 0.1.1

--- library/setup.py
+++ library/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='harbor3d',
-    version="0.1.0",
+    version="0.1.1",
     description="STL形式のファイルを出力可能な3Dモデリングツール",
     long_description="STL形式のファイルを出力可能な3Dモデリングツール\nコーディングで3Dモデルを作成する",
     url='',
